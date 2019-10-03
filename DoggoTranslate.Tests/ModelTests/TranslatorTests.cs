561632457e89f569d72ddc83bab9f1344180bd04
Ethan Samuels-Ellingson and Molly Stambaugh <molly@>
2019-10-03 15:45:27 -0700

    add more tests, doggo, etc

--- DoggoTranslate.Tests/ModelTests/TranslatorTests.cs
+++ DoggoTranslate.Tests/ModelTests/TranslatorTests.cs
@@ -12,14 +12,14 @@ namespace DoggoTranslator.Tests
         public void IsValidString_StringMeetsConditions_True()
         {
             // Arrange
-            string inputString = "Dogs hate cats";
+            string inputString = "dogs hate cats";
             // Act
             bool result = Translator.IsValidString(inputString);
             // Assert
             Assert.AreEqual(true, result);
         }
         [TestMethod]
-        public void UseDictionaryWithString_SuccessfulStringReplace_KeyValue()
+        public void UseDictionaryOnString_SuccessfulStringReplace_KeyValue()
         {
             // Arrange
             string inputString = "dogs";
@@ -28,6 +28,35 @@ namespace DoggoTranslator.Tests
             // Assert
             Assert.AreEqual(result, "doggos");
         }
-
+        [TestMethod]
+        public void CallDictionaryOnArray_ReplaceAllElementsInArray_Translation()
+        {
+            // Arrange
+            string inputString = "dogs hate cats";
+            // Act
+            string result = Translator.DoggoTranslation(inputString);
+            // Assert
+            Assert.AreEqual(result, "doggos hate cades ");
+        }
+        [TestMethod]
+        public void RemoveEAfterV_CheckForEAfterV_WordWithoutE()
+        {
+            // Arrange
+            string inputString = "have";
+            // Act
+            string result = Translator.RemoveEAfterV(inputString);
+            // Assert
+            Assert.AreEqual(result, "hav");
+        }
+        [TestMethod]
+        public void AddLastWord_AddLastWordToReturnString_LongerString()
+        {
+            // Arrange
+            string inputString = "I want cookies";
+            // Act
+            string resultAddWord = Translator.AddLastWord(Translator.DoggoTranslation(inputString));
+            // Assert
+            Assert.IsTrue (resultAddWord.Length > inputString.Length);
+        }
     }
 }
--- DoggoTranslate/Models/Translator.cs
+++ DoggoTranslate/Models/Translator.cs
@@ -6,7 +6,7 @@ namespace Doggo.Models
     public class Translator 
     {
         
-    private static Dictionary <string, string> DoggoWords = new Dictionary <string, string>() { {"dog", "doggo"}, {"dogs", "doggos"}, {"puppy", "pupper"}, {"puppies", "puppers"}, {"cat", "cade"},{"cats", "cades"}, {"human", "hooman"}, {"human", "hoomans"}, {"person", "hooman"}, {"people", "hoomans"} };
+    private static Dictionary <string, string> DoggoWords = new Dictionary <string, string>() { {"dog", "doggo"}, {"dogs", "doggos"}, {"puppy", "pupper"}, {"puppies", "puppers"}, {"cat", "cade"},{"cats", "cades"}, {"human", "hooman"}, {"humans", "hoomans"}, {"person", "hooman"}, {"people", "hoomans"} };
     
 
         public static bool IsValidString(string inputString)
@@ -19,10 +19,52 @@ namespace Doggo.Models
             }
             return result;
         }
+        public static string DoggoTranslation(string inputString)
+        {
+            string DoggoWordsReturn = ""; 
+            string[] words = inputString.Split(' ');
+
+            foreach (string word in words)
+            {
+                string oneWord = UseDictionary(word);
+                DoggoWordsReturn += $"{oneWord} ";
+            }
+
+            return DoggoWordsReturn;
+                            
+            // input string to array split at spaces
+            // forEach strings in array with UseDictionary 
+            // return DoggoWordsReturn strings in array separated by " " 
+
+        }
         public static string UseDictionary(string inputString)
         {
-            string result = " ";
+            string result = inputString;
+            if (DoggoWords.ContainsKey(inputString))
+            {
+                result = DoggoWords[inputString]; 
+                //get value from dictionary based on key
+                //store value in result string
+            }
+            
             return result;
         }
+        public static string RemoveEAfterV(string inputString)
+        {
+            string NoEWord = inputString;
+            if (inputString[inputString.Length - 1] == 'e' && (inputString[inputString.Length - 2] == 'v'))
+            {
+                NoEWord = inputString.Substring(0, inputString.Length - 1); 
+            }
+            return NoEWord; 
+        }
+        public static string AddLastWord(string inputString)
+        {
+            string[] lastWord = {". Karen pls.", ". blep", ". doin me a real significant bamboozle.", ". mlem", ". boop", ". chimken nuggers immediate.", ". heck Sharon", ". bork" }; 
+            Random random = new Random();
+            int index = random.Next(0, lastWord.Length);
+            return inputString + lastWord[index];
+        }
+
     }
 }
